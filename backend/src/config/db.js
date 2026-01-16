3ba10fac49957b8cfbb5aee663c7395cabbec243
Madala Mallikarjuna <22je0531@iitism.ac.in>
2026-01-16 14:42:07 +0530

    minor changes

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,14 +1,7 @@
 const { Pool } = require("pg");
-
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
-  ssl: {
-    rejectUnauthorized: false
-  }
+  connectionString: process.env.DATABASE_URL,
+  ssl: false,
 });
 
 module.exports = pool;
